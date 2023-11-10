289061a8636c4a760262f0cd22d2fc7187d7d4b8
Norbert Harrer <nharrer@equilaris.at>
2023-11-10 17:13:56 +0100

    First Version

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# wasteland-3-savegame-decoder
-Wasteland 3 Savegame Decoder
+# Wasteland 3 Save File Decoder
+
+## Features
+
+ * Extracts a Wasteland 3 save file into a human readable xml file.
+ * Repacks the edited file pack to a WL3 save file.
+
+## Requirements
+
+> pip install python-lzf
+
+## Steps
+
+ 1. Copy the save file to this folder.
+ 2. In save_extract.py: Replace filename (without .xml ending) and uncomment loadfile().
+ 3. Run the python script. A decoded file _SAVEGAMENAME_.xml_editable
+ is created.
+ 4. Edit _SAVEGAMENAME_.xml_editable
+ 5. In save_extract.py: comment out loadfile() and uncomment savefile()
+ 6. Run the script again, which creates SAVEGAMENAME_.xml_new
+ 7. Replace the original save file with this new file (make a backup first)
+
+## Credits
+
+Based on this [Reddit post](https://www.reddit.com/r/Wasteland/comments/ik4xn9/editing_save_files_stepbystep/) of an unknown Person.
+
+
+
+
---
+++ save_extract.py
@@ -0,0 +1,56 @@
+import lzf
+import re
+import xml.dom.minidom
+
+# Copy the save file to this directory.
+# Change the line below to reflect the file name (without the .xml extension).
+filename = "Test"
+
+
+def loadparts(skipContent = False):
+    with open(filename + ".xml", mode="rb") as f:
+        file = f.readlines()
+        size = int(file[4].strip().decode("utf-8").replace("DataSize:=", ""))
+        header = ""
+        content = b""
+        for i, line in enumerate(file):
+            if i <= 12:
+                header += line.decode("utf-8")
+            else:
+                content += line
+
+        if skipContent:
+            return [ header, '' ]
+
+        content = lzf.decompress(content, size).decode('utf-8')
+        dom = xml.dom.minidom.parseString(content)
+        content_pretty = dom.toprettyxml()
+
+        return [header, content_pretty]
+
+
+def loadfile():
+    [_, content] = loadparts(False)
+    with open(filename + ".xml_editable", mode="w", encoding='utf-8') as f:
+        f.write(content)
+
+
+def savefile():
+    [header, _] = loadparts(True)
+    with open(filename + ".xml_editable", mode="r") as f:
+        file = f.read()
+        sf_length = len(file)
+        content = lzf.compress(file)
+        sfc_length = len(content)
+        header = re.sub(r"^(DataSize:=)\d*", f"\\g<1>{sf_length}", header, flags=re.MULTILINE)
+        header = re.sub(r"^(SaveDataSize:=)\d*", f"\\g<1>{sfc_length}", header, flags=re.MULTILINE)
+        with open(filename + ".xml_new", mode="wb+") as f:
+            f.write(bytes(header, "utf-8"))
+            f.write(content)
+        print(f"DataSize: {sf_length} SaveDataSize: {sfc_length}")
+
+# uncomment this to create the ..._editable file.
+#loadfile()
+
+# uncomment this to conver the ..._editable file to the ..._new file.
+#savefile()
